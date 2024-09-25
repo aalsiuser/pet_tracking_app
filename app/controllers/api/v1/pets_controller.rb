ebaecae9cea9ef1f82718053842aa669c79814d4
aalsiuser <vamshee3000@gmail.com>
2024-09-25 21:55:57 +0200

    Add pets_controller and its routes

---
+++ app/controllers/api/v1/pets_controller.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # Controller to receive pet tracking information from trackers and also to query stored tracking information
+    class PetsController < ApplicationController
+      rescue_from Dry::Types::ConstraintError do |e|
+        render json: { error: e.message }, status: :unprocessable_entity
+      end
+
+      # GET /api/v1/pets
+      def index
+        result = SearchPets.new(**index_params).call
+
+        render json: result
+      end
+
+      # POST /api/v1/pets
+      def create
+        CreatePet.new(**create_params).call
+        render json: :success, status: :created
+      rescue KeyError => e
+        render json: { error: e.message }, status: :bad_request
+      end
+
+      # GET /api/v1/pets
+      def show
+        result = SearchPets.new(**search_params).call
+
+        render json: result
+      end
+
+      # GET /api/v1/pets/count
+      def count
+        result = CountPets.new(**count_params).call
+
+        render json: result
+      end
+
+      private
+
+      def create_params
+        params.require(:pet).permit(:pet_type, :tracker_type, :owner_id, :in_zone, :lost_tracker).to_h.symbolize_keys
+      end
+
+      def count_params
+        params.permit(:pet_type, :tracker_type).to_h.symbolize_keys
+      end
+
+      def search_params
+        params.permit(:id, :pet_type, :tracker_type, :n_zone, :lost_tracker).to_h.symbolize_keys
+      end
+    end
+  end
+end
